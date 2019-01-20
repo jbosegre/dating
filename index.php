906558f2f3639e937fd0c74d9a6ac0358bcb1c59
jbosegre <jbose@mail.greenriver.edu>
2019-01-19 23:59:30 -0500

    added fat free framework to index.php

--- index.php
+++ index.php
@@ -5,3 +5,24 @@
  * Date: 1/19/2019
  * Time: 8:52 AM
  */
+
+//Require the autoload file
+
+ini_set ("display_errors", 1);
+error_reporting(E_ALL);
+
+//Require the autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+$f3->set('DEBUG', 3);
+
+//define a default route
+$f3->route('GET /', function (){
+    $view = new View;
+    echo $view->render('views/home.html');
+});
+
+//Run fat Free
+$f3->run();
